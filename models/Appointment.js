b221091933f6a9a7f241da798d77c609a482a943
Sajees XPS <sajees.jeya@gmail.com>
2022-02-23 21:01:32 -0500

    appointment/time models

---
+++ models/Appointment.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Appointment extends Model {}
+
+Appointment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Appointments_time: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Time',
+        key: 'id',
+      },
+    },
+    Appointments_date: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Appointments_day: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Appointments_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    Appointments_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Appointments',
+  }
+);
+
+module.exports = Appointment;
