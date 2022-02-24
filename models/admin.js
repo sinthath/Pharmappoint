0d3a9a8fbc7ef12f2b549cb3eaf301549fc0dd4e
edirie <essedirie@gmail.com>
2022-02-23 21:25:29 -0500

    changes

--- models/admin.js
+++ models/admin.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../connections/dbconnection");
 const bcrypt = require('bcrypt');
 
-class Admin extends Model {}
+class Admin extends Model{
+    passwordCheck(passwordLogin) {
+        return bcrypt.compareSync(passwordLogin, this.password);
+    }
+}
 
 Admin.init(
   {
@@ -20,15 +24,30 @@ Admin.init(
       password: {
           type: DataTypes.STRING,
           allowNull: false,
+          vaidate :{
+              len: [8]
+          }
       }
+    },
+
+      {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
 
     },
 
-  {
+  
     sequelize,
     freezeTableName: true,
-    modelName: "patient",
+    modelName: "admin",
   }
 );
 
-module.exports = admin;
+module.exports = Admin;
