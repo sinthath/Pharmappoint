1fe30ed6c9eda6efc0f65a9db51a435a34beac30
sinthath <sinthathirath@gmail.com>
2022-02-26 08:22:06 -0500

    create account page

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/dbconnection");
+
+class User extends Model {}
+
+User.init(
+  {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+
+      },
+      password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dob: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    allergies: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+
+    }
+
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
